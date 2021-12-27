f390c07f4e0efd49955dbac3ae83f9022d35eeb5
houseofsam <samxsaid@gmail.com>
2021-12-27 04:17:52 -0500

    Set up react-router
    Install & set up routes to 4 simple components to get the ball rolling. In webpack.config, add historyApiFallback: true as a property in devServer object to let the server know routing will be handled via server side code & that it should return index.html for all routes

--- src/app.js
+++ src/app.js
@@ -1,6 +1,42 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter, Route } from 'react-router-dom';
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 
-ReactDOM.render(<p>Balling on a Budget</p>, document.getElementById('app'));
+const ExpenseDashboardPage = () => (
+  <div>
+    Dashboard component
+  </div>
+);
+
+const AddExpensePage = () => (
+  <div>
+    Expense page component
+  </div>
+);
+
+const EditExpensePage = () => (
+  <div>
+    Edit Expense page component
+  </div>
+);
+
+const HelpPage = () => (
+  <div>
+    Help page component
+  </div>
+);
+
+const routes = (
+  <BrowserRouter>
+    <div>
+      <Route path="/" component={ExpenseDashboardPage} exact={true} />
+      <Route path="/create" component={AddExpensePage} />
+      <Route path="/edit" component={EditExpensePage} />
+      <Route path="/help" component={HelpPage} />
+    </div>
+  </BrowserRouter>
+);
+
+ReactDOM.render(routes, document.getElementById('app'));
--- webpack.config.js
+++ webpack.config.js
@@ -29,7 +29,8 @@ module.exports = {
   devtool: 'cheap-module-eval-source-map',
   devServer: {
     // absolute path to public folder similar to above
-    contentBase: path.join(__dirname, 'public')
+    contentBase: path.join(__dirname, 'public'),
+    historyApiFallback: true
   }
 };
 
