b2c96a76717595acc172371500bbf9096f0672bb
houseofsam <samxsaid@gmail.com>
2021-12-26 23:38:50 -0500

    Set up expense tracker project

---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+// set up module bundler which allows us to break up file
+// entry point, where our application kicks off --> output
+
+// import built in node function path
+const path = require('path');
+
+module.exports = {
+  entry: './src/app.js',
+  output: {
+    path: path.join(__dirname, 'public'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [{
+      // use loader that was installed
+      loader: 'babel-loader',
+      // target files that end with .js with regex
+      test: /\.js$/,
+      exclude: /node_modules/
+    }, {
+      test: /\.s?css$/,
+      use: [
+        'style-loader',
+        'css-loader',
+        'sass-loader'
+      ]
+    }]
+  },
+  devtool: 'cheap-module-eval-source-map',
+  devServer: {
+    // absolute path to public folder similar to above
+    contentBase: path.join(__dirname, 'public')
+  }
+};
+
