3da0fa35dd2ea1d549f345715a74de075150ffd8
houseofsam <samxsaid@gmail.com>
2021-12-27 06:21:06 -0500

    Set up 404 page, link between routes, organize routes and components into separate files.

---
+++ src/components/AddExpensePage.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const AddExpensePage = () => (
+  <div>
+    Expense page component
+  </div>
+);
+
+export default AddExpensePage;
+
---
+++ src/components/EditExpensePage.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const EditExpensePage = () => (
+  <div>
+    Edit Expense page component
+  </div>
+);
+
+export default EditExpensePage;
