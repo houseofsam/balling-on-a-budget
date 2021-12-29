c3b61dc4d36158500a4c5b0e1784446b2c374ad6
houseofsam <samxsaid@gmail.com>
2021-12-28 19:27:52 -0500

    Modify ExpensePage component, corresponding route, and Header links

--- src/components/EditExpensePage.js
+++ src/components/EditExpensePage.js
@@ -1,9 +1,15 @@
 import React from 'react';
 
-const EditExpensePage = () => (
-  <div>
-    Edit Expense page component
-  </div>
-);
+const EditExpensePage = (props) => {
+  console.log(props);
+
+  return (
+    (
+      <div>
+        Editing the expense with id of {props.match.params.id}
+      </div>
+    )
+  )
+};
 
 export default EditExpensePage;
